4b0f263700a806197c8f01ba3f4e7e414486f157
Sty1ebender <102916700+Sty1ebender@users.noreply.github.com>
2022-12-06 14:41:45 +0300

    Add files via upload

---
+++ 1 задача.py
@@ -0,0 +1,5 @@
+firstNum = int(input("Введите первое число: "))
+secondNum = int(input("Введите второе число: "))
+print(firstNum + secondNum, firstNum - secondNum, 
+firstNum * secondNum,  firstNum / secondNum, 
+firstNum ** secondNum, firstNum % secondNum, firstNum // secondNum)
---
+++ 4 задача.py
@@ -0,0 +1,3 @@
+import math
+radius = float(input("Введите радиус, пожалуйста: "))
+print(math.pi * (radius ** 2))
