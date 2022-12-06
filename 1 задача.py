6c9bad68ec6549b43cfa21008c4561f5a6ad7962
Sty1ebender <102916700+Sty1ebender@users.noreply.github.com>
2022-12-06 14:46:43 +0300

    Update 1 задача.py

--- 1 задача.py
+++ 1 задача.py
@@ -1,5 +1,3 @@
 firstNum = int(input("Введите первое число: "))
 secondNum = int(input("Введите второе число: "))
-print(firstNum + secondNum, firstNum - secondNum, 
-firstNum * secondNum,  firstNum / secondNum, 
-firstNum ** secondNum, firstNum % secondNum, firstNum // secondNum)
+print(firstNum + secondNum, firstNum - secondNum, firstNum * secondNum,  firstNum / secondNum, firstNum ** secondNum, firstNum % secondNum, firstNum // secondNum)
